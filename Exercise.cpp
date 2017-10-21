be66f67563754eb74824edcff9c16b6b9ae2bc5c
Maximilian Mayerl <maximilian.mayerl@student.uibk.ac.at>
2017-10-21 14:47:51 +0200

    Added midpoint implementation.

--- Exercise.cpp
+++ Exercise.cpp
@@ -236,6 +236,82 @@ void TimeStep(double dt, Scene::Method method,
         }
 
         case Scene::MIDPOINT: {
+            //Save old positions and velocities
+            Vec2 oldVelocities[points.size()];
+            Vec2 oldPositions[points.size()];
+
+            //Update positions and velocities to half-step
+            for (int i = 0; i < points.size(); i++) {
+                Point& p = points[i];
+
+                if (p.isFixed())
+                    continue;
+
+                //Compute acceleration at t
+                Vec2 a_t = (p.getForce() - p.getDamping() * p.getVel()) / p.getMass();
+
+                //Compute velocity at t + h/2
+                Vec2 v_temp = p.getVel() + dt/2 * a_t;
+
+                //Compute position at t + h/2
+                Vec2 x_temp = p.getPos() + dt/2 * p.getVel();
+
+                //Overwrite values in point and save old ones
+                oldVelocities[i] = p.getVel();
+                oldPositions[i] = p.getPos();
+
+                p.setVel(v_temp);
+                p.setPos(x_temp);
+                
+                //Reset forces
+                p.setForce(Vec2(0, -9.81 *  p.getMass()));
+                p.addForce(-p.getVel() * p.getDamping());
+
+                //Collision penalty
+                double penaltyStiffness = 1000;
+                double groundHeight = -2;
+                if (p.getPos().y < groundHeight) {
+                    double penetrationDepth = p.getPos().y - groundHeight;
+                    Vec2 penaltyForce = Vec2(0, 1) * penaltyStiffness * -penetrationDepth;
+                    p.addForce(penaltyForce);
+                }
+            }
+
+            //Add spring forces
+            for (Spring& s : springs) {
+                s.applyForce();
+                s.applyDamping();
+            }
+
+            //Update positions and velocities for full step
+            for (int i = 0; i < points.size(); i++) {
+                Point& p = points[i];                
+                if (p.isFixed())
+                    continue;
+
+                //Compute acceleration at t + h/2
+                Vec2 a_temp = (p.getForce() - p.getDamping() * p.getVel()) / p.getMass();
+
+                //Compute position at t + h
+                Vec2 x = oldPositions[i] + dt * p.getVel();
+
+                //Compute velocity at t + h
+                Vec2 v = oldVelocities[i] + dt * a_temp;
+
+                //Set new positions and velocities
+                p.setPos(x);
+                p.setVel(v);
+
+                //Reset forces
+                p.setForce(Vec2(0, -9.81 *  p.getMass()));
+                p.addForce(-p.getVel() * p.getDamping());
+            }
+
+            //Add spring forces again so they are correct at the beginning of the next time step
+            for (Spring& s : springs) {
+                s.applyForce();
+            }
+
             break;
         }
     }
