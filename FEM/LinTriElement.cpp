a18a60c8668c67b12db247a27375996b37e8370e
Alex Schlögl <alexander.schloegl@student.uibk.ac.at>
2017-11-16 13:07:13 +0100

    Changed the repo structure to contain multiple asignment.
    I also added the skeleton for the second assignment.

---
+++ FEM/LinTriElement.cpp
@@ -0,0 +1,28 @@
+/******************************************************************
+*                                                                  
+* LinTriElement.cpp
+*
+* Description: Implementation of functions for handling linear 
+* triangular elements
+*
+* Physically-Based Simulation Proseminar WS 2015
+*
+* Interactive Graphics and Simulation Group
+* Institute of Computer Science
+* University of Innsbruck
+*
+*******************************************************************/
+
+#include <math.h>
+#include "FEModel.h"
+
+void LinTriElement::ComputeBasisDeriv(int nodeId, Vector2 &basisDeriv, const FEModel *model) const
+{
+    // Task 1   
+}
+
+void LinTriElement::AssembleElement(FEModel *model) const
+{
+    // Task 2
+}
+
