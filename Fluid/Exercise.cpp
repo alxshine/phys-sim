e033fc54bd0769d218e7c0a37fb0d76891eee88e
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2017-12-14 11:24:45 +0100

    Added task 1

--- Fluid/Exercise.cpp
+++ Fluid/Exercise.cpp
@@ -1,42 +1,65 @@
 /******************************************************************
-*
-* Exercise.cpp
-*
-* Description: In this file the functions AdvectWithSemiLagrange(),
-* SolvePoisson(), and CorrectVelocities() have to be implemented
-* for the third programming assignment.
-* Feel free to add new local functions into this file.
-* Changes to other source files of the framework should not be
-* required. 
-*
-* Physically-Based Simulation Proseminar WS 2016
-*
-* Interactive Graphics and Simulation Group
-* Institute of Computer Science
-* University of Innsbruck
-*
-*******************************************************************/
-
+ *
+ * Exercise.cpp
+ *
+ * Description: In this file the functions AdvectWithSemiLagrange(),
+ * SolvePoisson(), and CorrectVelocities() have to be implemented
+ * for the third programming assignment.
+ * Feel free to add new local functions into this file.
+ * Changes to other source files of the framework should not be
+ * required.
+ *
+ * Physically-Based Simulation Proseminar WS 2016
+ *
+ * Interactive Graphics and Simulation Group
+ * Institute of Computer Science
+ * University of Innsbruck
+ *
+ *******************************************************************/
 
 #include "Fluid2D.h"
 
-void AdvectWithSemiLagrange(int xRes, int yRes, double dt, 
-                            double *xVelocity, double *yVelocity, 
-                            double *field, double* tempField)
-{
-    // Task 1
+void AdvectWithSemiLagrange(int xRes, int yRes, double dt, double *xVelocity,
+		double *yVelocity, double *field, double* tempField) {
+	// Task 1
+	for (int y = 0; y < yRes; y++) {
+		for (int x = 0; x < xRes; x++) {
+			//calculate old position based on current velocity
+			double oldX = x - xVelocity[y * xRes + x] * dt;
+			double oldY = y - yVelocity[y * xRes + x] * dt;
+
+			//interpolate quantity from neighbors of old coordinates
+			int leftCoord = (int) oldX;
+			int rightCoord = leftCoord + 1;
+			int bottomCoord = (int) oldY;
+			int topCoord = bottomCoord + 1;
+
+			double bottomLeft = tempField[bottomCoord * xRes + leftCoord];
+			double bottomRight = tempField[bottomCoord * xRes + rightCoord];
+			double topLeft = tempField[topCoord * xRes + leftCoord];
+			double topRight = tempField[topCoord * xRes + rightCoord];
+
+			double xRatio = oldX - leftCoord;
+			double yRatio = oldY - bottomCoord;
+
+			double leftInterpolation = topLeft * yRatio
+					+ bottomLeft * (1 - yRatio);
+			double rightInterpolation = topRight * yRatio
+					+ bottomRight * (1 - yRatio);
+
+			field[y * xRes + x] = rightInterpolation * xRatio
+					+ leftInterpolation * (1 - xRatio);
+		}
+	}
 }
 
-
-void SolvePoisson(int xRes, int yRes, int iterations, double accuracy, 
-                  double* pressure, double* divergence)
-{
-    // Task 2
+void SolvePoisson(int xRes, int yRes, int iterations, double accuracy,
+		double* pressure, double* divergence) {
+// Task 2
 }
 
-void CorrectVelocities(int xRes, int yRes, double dt, const double* pressure, 
-                       double* xVelocity, double* yVelocity)
-{
-    // Task 3  
+void CorrectVelocities(int xRes, int yRes, double dt, const double* pressure,
+		double* xVelocity, double* yVelocity) {
+// Task 3
 }
 
