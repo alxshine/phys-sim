14213a8723337a779728601b02d9b296b763db52
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2017-12-14 16:49:39 +0100

    Finished, should be about right

--- Fluid/Exercise.cpp
+++ Fluid/Exercise.cpp
@@ -16,6 +16,8 @@
  * University of Innsbruck
  *
  *******************************************************************/
+#include <limits>
+#include <cmath>
 
 #include "Fluid2D.h"
 
@@ -61,17 +63,48 @@ void SolvePoisson(int xRes, int yRes, int iterations, double accuracy,
 	double h = 1.0 / xRes;
 
 	for (int i = 0; i < iterations; i++) {
+		//solver iteration
 		for (int y = 0; y < yRes; y++) {
 			for (int x = 0; x < xRes; x++) {
 				int c = y * xRes + x;
-				pressure[c] =
-						(h * h * divergence[c] + pressure[c + xRes]
-								+ pressure[c - xRes] + pressure[c + x]
-								+ pressure[c - x]) / 4;
+				//handle boundaries
+				double above = y < yRes - 1 ? pressure[c + xRes] : 0;
+				double below = y > 0 ? pressure[c - xRes] : 0;
+				double left = x > 0 ? pressure[c - 1] : 0;
+				double right = x < xRes - 1 ? pressure[c + 1] : 0;
+//				double above = pressure[c + xRes];
+//				double below = pressure[c - xRes];
+//				double left = pressure[c - 1];
+//				double right = pressure[c + 1];
+
+				pressure[c] = (h * h * divergence[c] + above + below + left
+						+ right) / 4;
 			}
 		}
+
+		//compute residual
+		double residual = 0.;
+		for (int y = 0; y < yRes; y++) {
+			for (int x = 0; x < xRes; x++) {
+				int c = y * xRes + x;
+				//handle boundaries
+				double above = y < yRes - 1 ? pressure[c + xRes] : 0;
+				double below = y > 0 ? pressure[c - xRes] : 0;
+				double left = x > 0 ? pressure[c - 1] : 0;
+				double right = x < xRes - 1 ? pressure[c + 1] : 0;
+//				double above = pressure[c + xRes];
+//				double below = pressure[c - xRes];
+//				double left = pressure[c - 1];
+//				double right = pressure[c + 1];
+
+				double rTemp = h * h * divergence[c] + above + below + left
+						+ right - 4 * pressure[c];
+				residual += rTemp * rTemp;
+			}
+		}
+		if (sqrt(residual) < accuracy)
+			return;
 	}
-	//TODO: add break on achieved accuracy
 }
 
 void CorrectVelocities(int xRes, int yRes, double dt, const double* pressure,
